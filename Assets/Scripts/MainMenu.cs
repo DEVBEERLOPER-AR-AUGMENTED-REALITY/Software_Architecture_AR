fb01fbe29d036fa587d8d8433b3b0c162bbd118c
Caivan <caivan2@gmail.com>
2019-12-27 13:37:46 -0500

    Version 1.0.0 APP

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{   
+    public void startADD () {
+        SceneManager.LoadScene ("SceneADD");
+    }
+
+    public void startACDM () {
+        SceneManager.LoadScene ("SceneACDM");
+    }
+
+    public void startRozanskiAndWoods () {
+        SceneManager.LoadScene ("SceneRozanskiWoods");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NavigationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Make sure user is on Android platform
+    if (Application.platform == RuntimePlatform.Android) {
+        
+        // Check if Back was pressed this frame
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            SceneManager.LoadScene ("SceneACDM");
+        }
+    }
+        
+    }
+}
